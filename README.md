a627b55d90019b7170da75fb57fe01cab2ee4530
Zegedam Zegeye <zegedam@gmail.com>
2020-03-20 15:01:58 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Bang! Board game
---
+++ game2/Game2.java
@@ -0,0 +1,20 @@
+/*
+ * CS 2365 OOP Spring 2020
+ * Zegedam Zegeye
+ */
+package game2;
+
+/**
+ *
+ * @author zeged
+ */
+public class Game2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
