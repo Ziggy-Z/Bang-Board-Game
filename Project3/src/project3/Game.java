31fe9ab12faec722519e99d83f888768f18a285d
nathanclough <nathan.clough@ttu.edu>
2020-03-21 12:45:43 -0500

    Game now has creates roles and sets up characters correctly and added testClientGame.java which verifys that those functions work correctly

--- Project3/src/project3/Game.java
+++ Project3/src/project3/Game.java
@@ -1,6 +1,7 @@
 package project3;
+import java.util.Collections;
 import java.util.ArrayList;
-import java.util.ListIterator;
+import java.util.LinkedList;
 /*
 CS 2365 OOP Spring 2020 Section 2
 Nathan Clough
@@ -12,18 +13,75 @@ Nathan Clough
  */
 public class Game {
     ArrayList<Player> players = new ArrayList<Player>();
+    LinkedList<String> roles = new LinkedList<String>();
+    LinkedList<Integer> characters = new LinkedList<Integer>();
     boolean finished;
     
     public Game(int numPlayers)
     {  
+        
         for(int i =1; i <=numPlayers; i++)
         {
             Player p = new Player(i); 
             players.add(p); 
-            
-               
         }
+        setupRoles();
+        assignCharacters();
+        
     }
+    public void assignCharacters(){
+        for (int i =1; i<=16; i++ )
+           characters.add(i);
+        Collections.shuffle(characters);
+        for(Player t : players)
+            t.setCharacter(characters.pop());
+        
+    }
+    /**
+     * Based on the number of players this method will create a list of roles 
+     * then shuffle them and assign to the players
+     */
+    public void setupRoles(){
+        //special rules for 3 people
+        if(players.size() == 3)
+        {
+            roles.add("Deputy");
+            roles.add("Renegade");
+            roles.add("Outlaw");
+        }
+        else{
+        //always have one sheriff
+        roles.add("Sheriff");
+        
+        //one renegade unless there are 8 players
+        roles.add("Renegade");
+        if(players.size() > 7)
+            roles.add("Renegade");
+        
+        //2 outlaws unless there is more than 5 players
+        int numOutlaws = 2;
+        if (players.size() > 5)
+            numOutlaws ++;
+        for (int i = 0; i< numOutlaws; i++)
+            roles.add("Outlaw");
+        
+        //one deputy unles there are 7 or 8 players
+        if(players.size() > 4)
+            roles.add("Deputy");
+        if(players.size() > 6)
+            roles.add("Deputy");
+                }
+        //shuffles the list of roles 
+        Collections.shuffle(roles);
+                
+        //takes a role and assigns it to each player
+        for(Player t: players )
+            t.setRole(roles.pop());
+
+    }
+    /**
+     * Function for looping through the players arrayList and having each player take turn in order
+     */
     public void play(){
 //loops through the list allowing to keep taking turns in the correct order
     int i = 0;    
@@ -48,6 +106,13 @@ public class Game {
             finished = true;
         }
         
+    }
+    public int getNumPlayers()
+    {
+        return players.size() + 1;
+    }
+    public ArrayList<Player> getPlayers(){
+        return players;
     }
     public void display()
     {
