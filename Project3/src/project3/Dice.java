39568b91496624cb4458950a4cc04dfcbcb2e443
Krystyna Urbanczyk <krystyna.urbanczyk@ttu.edu>
2020-03-22 14:14:48 -0500

    first commit addded the dice class

---
+++ Project3/src/project3/Dice.java
@@ -0,0 +1,25 @@
+// CS 2365 OOP Spring 2020 Section 002
+//Krystyna Urbanczyk
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project3;
+import java.util.Random;
+
+/**
+ *
+ * @author krystynaurbanczyk
+ */
+
+public class Dice {
+    public char dice1;
+    public char dice2;
+    public char dice3;
+    public char dice4;
+    public char dice5;
+    
+    
+}
