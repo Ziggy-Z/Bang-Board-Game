fca6cc29bebca61f18c0c79a64af81a54ebaaa08
Krystyna Urbanczyk <krystyna.urbanczyk@ttu.edu>
2020-03-23 10:47:55 -0500

    completed dice class with test cases

--- Project3/src/project3/Dice.java
+++ Project3/src/project3/Dice.java
@@ -1,11 +1,6 @@
-// CS 2365 OOP Spring 2020 Section 002
+// CS 2365 OOP Section 002 Spring 2020 
 //Krystyna Urbanczyk
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package project3;
 import java.util.Random;
 
@@ -15,11 +10,49 @@ import java.util.Random;
  */
 
 public class Dice {
-    public char dice1;
-    public char dice2;
-    public char dice3;
-    public char dice4;
-    public char dice5;
     
+    public int dice;
+    public String result;
+    
+    public Dice(){
+        dice = 0;
+        result = null;
+    }
+    
+    public String rollDie(){
+        
+        Random diceface = new Random();
+        int face = diceface.nextInt((6 - 1) + 1) + 1;
+        
+        //int temp = 0;
+        switch (face) {
+            case 1:
+                result = "Arrow";
+                break;
+            case 2:
+                result = "Dynamite";
+                break;
+            case 3:
+                result = "One";
+                break;
+            case 4:
+                result = "Two";
+                break;
+            case 5:
+                result = "Gatling";
+                break;
+            case 6:
+                result = "Beer";
+                break;
+            default:
+                break;
+        }
+        
+        return result;
+    }
+    
+    public String getResult(){
+        return result;
+    }
     
 }
