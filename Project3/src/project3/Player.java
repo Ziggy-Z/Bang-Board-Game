41d17d90150484f804050b98e3f0528952e07e28
Joseph-McClaine <joseph.mcclaine@ttu.edu>
2020-03-27 18:14:16 -0500

    finished Player class

--- Project3/src/project3/Player.java
+++ Project3/src/project3/Player.java
@@ -5,11 +5,100 @@ package project3;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.*;
+import java.nio.file.*;
 /**
- *
- * @author bigjo
+ * @author Joseph McClaine
  */
 public class Player {
     
+    private int CurrentHealth;
+    private int MaxHealth;
+    private int Arrows=0;
+    private String Role;
+    private String Character;
+    private String ability;
+    private boolean AI;
+    
+    
+    public Player(boolean temp)
+    {
+        this.AI=temp;
+        /*
+        setCharacterTraits(0);
+        System.out.println("Name: " +Character);
+        System.out.println("Max Health: " +MaxHealth);
+        System.out.println("Current Health: " +CurrentHealth);
+        System.out.println("Ability: " +ability);
+        setRole("Bandit");
+        setHealth(10);
+        setArrows(5);
+        System.out.println("Current Health: " +CurrentHealth);
+        System.out.println("Role: " +Role);
+        System.out.println("Arrows: "+Arrows);
+*/
+    }
+    public int getHealth()
+    {
+        return CurrentHealth;
+    }
+    public void setHealth(int num)
+    {
+        CurrentHealth=num;
+    }
+    public int getArrows()
+    {
+        return Arrows;
+    }
+    public void setArrows(int num)
+    {
+        Arrows=num;
+    }
+    public String getRole()
+    {
+        return Role;
+    }
+    public void setRole(String temp)
+    {
+        Role=temp;
+    }
+    public String getCharacter()
+    {
+        return Character;
+    }
+    public String getAbility()
+    {
+        return ability;
+    }
+    public boolean isFullHealth()
+    {
+        if (CurrentHealth == MaxHealth)
+            return true;
+        else
+            return false;
+    }
+    public void setCharacterTraits(int num)
+    {
+        String temp="";
+        try{
+            temp = Files.readAllLines(Paths.get("Bang_Characters.txt")).get(num);
+            AssignTraits(temp);
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+    private void AssignTraits(String temp)
+    {
+        String[] arrtemp = temp.split(";",0);
+        Character = arrtemp[0].toUpperCase();
+        MaxHealth = Integer.parseInt(arrtemp[1]);
+        CurrentHealth = MaxHealth;
+        ability = arrtemp[2];
+    }  
+    public boolean isAI()
+    {
+        return AI;
+    }
 }
