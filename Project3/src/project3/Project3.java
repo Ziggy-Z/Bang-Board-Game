6e22adc3873f8dd43ff9ec685eb7378b1e276335
nathanclough <nathan.clough@ttu.edu>
2020-03-30 09:32:30 -0500

    added main for z

---
+++ Project3/src/project3/Project3.java
@@ -0,0 +1,41 @@
+/*
+CS 2365 OOP Spring 2020 Section 2
+Nathan Clough
+ */
+package project3;
+import Testing.TestClientGame;
+/**
+ *
+ * @author X1Gen3
+ */
+public class Project3 {
+//create a lock .notifyb 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+       
+        
+        //Displays the board 
+
+       
+ 
+       TestClientGame test = new TestClientGame();
+       test.checkCharacters();
+       
+        
+        
+        /*
+       Object lock = new Object();
+       int x;
+       UI input = new UI();
+
+
+       x = input.getStartNumPlayers();
+   
+       
+       System.out.println(x);*/
+           
+    }
+    
+}
