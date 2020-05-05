a8353de22981ee2309c06c5bbde49ceb04d7e4dc
Joseph-McClaine <joseph.mcclaine@ttu.edu>
2020-05-05 15:34:11 -0500

    Added Java Docs

--- Project3/src/project3/ChiefArrow.java
+++ Project3/src/project3/ChiefArrow.java
@@ -1,6 +1,4 @@
-
 package project3;
-
 /**
  *
  * @author Joseph McClaine
@@ -16,19 +14,50 @@ public class ChiefArrow {
     {
       Create_Deck();
     }
+/**
+* Create_Deck creates the Array List for the list of Arrows
+* Contributors: None
+* @param void
+* @author Joseph McClaine
+* @return void
+*/
     private void Create_Deck()
     {
         Reset_Arrows();
     }
+    
+/**
+* getArrow pulls an arrow from the list and value from Array list is returned to see if it was Chief Arrow or Regular Arrow
+* Contributors: None
+* @param void
+* @author Joseph McClaine
+* @return int
+*/
     public int getArrow()
     {
         int x = Arrow_List.remove(0);
        return x;
     }
+    
+/**
+* getNum_Arrows returns the number of arrows left in the ArrayList to see if there are arrows left
+* Contributors: None
+* @param void
+* @author Joseph McClaine
+* @return int
+*/
     public int getNum_Arrows()
     {
         return Arrow_List.size();
     }
+    
+/**
+* Shuffle_Arrows Shuffles the List of Arrows so the Chief Arrow is in list Randomly
+* Contributors: None
+* @param void
+* @author Joseph McClaine
+* @return void
+*/
     private void Shuffle_Arrows()
     {
         for(int i=0; i<Arrow_List.size(); i++)
@@ -38,6 +67,14 @@ public class ChiefArrow {
             Collections.swap(Arrow_List, i, temp);
         }
     }
+    
+/**
+* Reset_Arrows refills the arrows in the list and shuffles them again
+* Contributors: None
+* @param void
+* @author Joseph McClaine
+* @return void
+*/
     public void Reset_Arrows()
     {
         for (int i=1; i<=Regular_Arrow; i++)
@@ -47,20 +84,4 @@ public class ChiefArrow {
         Arrow_List.add(1);
         Shuffle_Arrows();
     }
-    /*
-    public static void main(String[] args)
-    {
-        ChiefArrow d = new ChiefArrow(1);
-        int temp= d.getNum_Arrows();
-        for (int i=0; i<temp; i++)
-        {
-            System.out.println("Number: "+d.getArrow());
-        }
-        System.out.println("\n\nNew Spotn\n\n");
-        d.Reset_Arrows();
-        for (int i=0; i<temp; i++)
-        {
-            System.out.println("Number: "+d.getArrow());
-        }
-    } */
 }
--- Project3/src/project3/Player.java
+++ Project3/src/project3/Player.java
@@ -44,41 +44,121 @@ public class Player {
     public String getNumber(){
         return number;
     }
+    
+/**
+* getHealth returns the int value of the player's current health 
+* Contributors: None
+* @param void
+* @author Joseph McClaine
+* @return int
+*/
     public int getHealth()
     {
         return CurrentHealth;
     }
+    
+/**
+* setUser sets the AI to false to indicate that the player is a user 
+* Contributors: None
+* @param None
+* @author Joseph McClaine
+* @return void
+*/
     public void setUser(){
         AI = false;
     }
+    
+/**
+* setHealth sets the current health to the int parameter 
+* Contributors: None
+* @param int
+* @author Joseph McClaine
+* @return void
+*/
     public void setHealth(int num)
     {
         CurrentHealth=num;
     }
+    
+/**
+* getArrows Returns the number of arrows that the user has 
+* Contributors: None
+* @param none
+* @author Joseph McClaine
+* @return int
+*/
     public int getArrows()
     {
         return Arrows;
     }
+    
+/**
+* setArrows sets the current number of arrows to the int parameter 
+* Contributors: None
+* @param int
+* @author Joseph McClaine
+* @return void
+*/
     public void setArrows(int num)
     {
         Arrows=num;
     }
+    
+/**
+* getRole returns a string of the character's set role
+* Contributors: None
+* @param none
+* @author Joseph McClaine
+* @return String
+*/
     public String getRole()
     {
         return Role;
     }
+    
+/**
+* setRole sets the role string to the parameter string
+* Contributors: None
+* @param String
+* @author Joseph McClaine
+* @return void
+*/
     public void setRole(String temp)
     {
         Role=temp;
     }
+    
+/**
+* getCharacter returns a String that contains name of the character
+* Contributors: None
+* @param None
+* @author Joseph McClaine
+* @return String
+*/
     public String getCharacter()
     {
         return Character;
     }
+    
+/**
+* getAbility returns the string of the character's special ability
+* Contributors: None
+* @param None
+* @author Joseph McClaine
+* @return String
+*/
     public String getAbility()
     {
         return ability;
     }
+    
+/**
+* isFullHealth returns Boolean of whether the character is at full health
+* Contributors: None
+* @param None
+* @author Joseph McClaine
+* @return boolean
+*/
     public boolean isFullHealth()
     {
         if (CurrentHealth == MaxHealth)
@@ -86,6 +166,14 @@ public class Player {
         else
             return false;
     }
+    
+/**
+* setCharacterTraits pulls a character and traits using parameter as the line number to pull from in text document
+* Contributors: Nathan Clough
+* @param int
+* @author Joseph McClaine
+* @return void
+*/
     public void setCharacterTraits(int num)
     {
         String temp="";
@@ -98,6 +186,14 @@ public class Player {
             System.out.println(e);
         }
     }
+    
+/**
+* AssignTraits receives the string from the text document and breaks it down into parts
+* Contributors: None
+* @param String
+* @author Joseph McClaine
+* @return void
+*/
     private void AssignTraits(String temp)
     {
         String[] arrtemp = temp.split(";",0);
@@ -106,15 +202,39 @@ public class Player {
         CurrentHealth = MaxHealth;
         ability = arrtemp[2];
         ability = ability.replace(":","\n");
-    }  
+    }
+    
+/**
+* isAI returns boolean whether the player is AI or user
+* Contributors: None
+* @param None
+* @author Joseph McClaine
+* @return boolean
+*/
     public boolean isAI()
     {
         return AI;
     }
+    
+/**
+* isChief_Arrow returns boolean saying whether the player has the Chief Arrow token
+* Contributors: None
+* @param None
+* @author Joseph McClaine
+* @return boolean
+*/
     public boolean isChief_Arrow()
     {
         return Chief_Arrow;
     }
+    
+/**
+* setChief_Arrow sets the Chief_Arrow to the parameter passed to add or remove Chief Arrow token from the player
+* Contributors: None
+* @param boolean
+* @author Joseph McClaine
+* @return void
+*/
     public void setChief_Arrow(boolean Choice)
     {
         Chief_Arrow = Choice;
